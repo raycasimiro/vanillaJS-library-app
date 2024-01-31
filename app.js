a6d0f5c38b350eeb7c58377308bb4deb9917e8cc
raycasimiro <54532910+raycasimiro@users.noreply.github.com>
2024-01-31 20:04:51 +0800

    new commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const myLibray = [];
+let myLibray = [];
 
 class Book {
   constructor(
@@ -18,34 +18,47 @@ class Book {
 
 const formInput = document.getElementById("form-book-details");
 const booksContainer = document.getElementById("books-container");
-
 const dialog = document.querySelector("dialog");
+const btnDismissDialog = document.getElementById("btn-dismiss-dialog");
 
-// btnOpenDialog.addEventListener("click", (e) => {
-//   dialog.showModal();
-// });
-
-dialog.addEventListener("click", (e) => {
-  if (e.target.tagName === "DIALOG") dialog.close();
-});
-
-formInput.addEventListener("submit", (e) => {
-  e.preventDefault();
-  myLibray.push(getInput());
-  refreshBooksContainer();
-  populateBooksContainer();
+btnDismissDialog.addEventListener("click", () => {
   dialog.close();
   formInput.reset();
 });
 
+dialog.addEventListener("click", (e) => {
+  if (e.target.tagName === "DIALOG") {
+    dialog.close();
+    formInput.reset();
+  }
+});
+
+formInput.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let newBook = getInput();
+  if (newBook !== null) {
+    myLibray.push(newBook);
+    refreshBooksContainer();
+    populateBooksContainer();
+    dialog.close();
+    formInput.reset();
+  } else alert("Title already exist!");
+});
+
 const getInput = () => {
   let title = document.getElementById("form-book-title").value;
+  if (isExistingTitle(title)) return null;
+
   let author = document.getElementById("form-book-author").value;
   let review = document.getElementById("form-book-review").value;
   let pageCount = document.getElementById("form-book-page-count").value;
   let isRead = document.getElementById("form-book-is-read").checked;
-
   return new Book(title, author, review, pageCount, isRead);
+};
+
+const isExistingTitle = (title) => {
+  for (let book of myLibray) if (book.title === title) return true;
+  return false;
 };
 
 const refreshBooksContainer = () => {
@@ -83,23 +96,43 @@ const createBookCard = (book) => {
   let bookAuthor = document.createElement("small");
   let bookReview = document.createElement("p");
   let bookPageCount = document.createElement("small");
+  let btnRemoveBook = document.createElement("button");
+  let isReadIcon = document.createElement("div");
 
   bookCard.classList.add("book-card");
   bookTitle.classList.add("book-card-title");
   bookAuthor.classList.add("book-card-author");
   bookReview.classList.add("book-card-review");
   bookPageCount.classList.add("book-card-page-count");
+  btnRemoveBook.classList.add("btn-remove-book");
+  isReadIcon.classList.add("icon-is-read");
 
   bookTitle.textContent = `${book.title}`;
   bookAuthor.textContent = `by ${book.author}`;
   bookReview.textContent = `${book.review}`;
   bookPageCount.textContent = `Page count: ${book.pageCount}`;
+  btnRemoveBook.innerHTML = '<ion-icon name="close"></ion-icon>';
+  isReadIcon.innerHTML = '<ion-icon name="checkmark-circle"></ion-icon>';
+  btnRemoveBook.onclick = removeBookCard;
 
   bookCard.appendChild(bookTitle);
   bookCard.appendChild(bookAuthor);
   bookCard.appendChild(bookReview);
   bookCard.appendChild(bookPageCount);
+  if (book.isRead) bookCard.appendChild(isReadIcon);
+  bookCard.appendChild(btnRemoveBook);
   booksContainer.appendChild(bookCard);
+};
+
+const removeBookCard = (e) => {
+  let title = e.target.parentNode.querySelector("h2").textContent;
+
+  myLibray = myLibray.filter((book) => book.title !== title);
+
+  for (let book of myLibray) if (book.title !== title) console.log(book.title);
+
+  refreshBooksContainer();
+  populateBooksContainer();
 };
 
 myLibray.push(
@@ -118,7 +151,7 @@ myLibray.push(
     "Jonathan Swift",
     "Everyone remembers poor Gulliver in breeches and three-cornered hat, pinned down with cords on a beach, by an army of minute soldiers. A young boy's nightmare, no doubt, but there is much more to this book than this rosy image, reproduced endlessly on the pediments of toy shops and theme parks. This is indeed an astonishing book.",
     306,
-    true
+    false
   )
 );
 
